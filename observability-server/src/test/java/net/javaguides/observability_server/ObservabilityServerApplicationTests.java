fadbfa5a34ee23eb60f95d5354eb2033ff9dbbdb
chemadh <111968391+chemadh@users.noreply.github.com>
2024-11-27 23:51:58 +0100

    Add files via upload

---
+++ observability-server/src/test/java/net/javaguides/observability_server/ObservabilityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.observability_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
