14f45542533bc9ca7be1a13cf885d2558fa209a2
chemadh <111968391+chemadh@users.noreply.github.com>
2024-11-27 23:50:11 +0100

    Add files via upload

---
+++ observability-client/src/main/java/net/javaguides/observability_client/ObservabilityClientApplication.java
@@ -0,0 +1,76 @@
+package net.javaguides.observability_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ObservabilityClientApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(ObservabilityClientApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservabilityClientApplication.class, args);
+	}
+
+	// tag::rest-template[]
+	// IMPORTANT! To instrument RestTemplate you must inject the RestTemplateBuilder
+	@Bean
+	RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	// end::rest-template[]
+
+	// tag::runner[]
+	@Bean
+	CommandLineRunner myCommandLineRunner(ObservationRegistry registry, RestTemplate restTemplate) {
+		Random highCardinalityValues = new Random(); // Simulates potentially large number of values
+		List<String> lowCardinalityValues = Arrays.asList("userType1", "userType2", "userType3"); // Simulates low number of values
+		return args -> {
+			// let's simulate endless traffic
+			while(true) {
+				String highCardinalityUserId = String.valueOf(highCardinalityValues.nextLong(100_000));
+				// Example of using the Observability API manually
+				// <my.observation> is a "technical" name that does not depend on the context. It will be used to name e.g. Metrics
+				Observation.createNotStarted("my.observation", registry)
+						// Low cardinality means that the number of potential values won't be big. Low cardinality entries will end up in e.g. Metrics
+						.lowCardinalityKeyValue("userType", randomUserTypePicker(lowCardinalityValues))
+						// High cardinality means that the number of potential values can be large. High cardinality entries will end up in e.g. Spans
+						.highCardinalityKeyValue("userId", highCardinalityUserId)
+						// <command-line-runner> is a "contextual" name that gives more details within the provided context. It will be used to name e.g. Spans
+						.contextualName("command-line-runner")
+						// The following lambda will be executed with an observation scope (e.g. all the MDC entries will be populated with tracing information). Also the observation will be started, stopped and if an error occurred it will be recorded on the observation
+						.observe(() -> {
+							log.info("Will send a request to the server"); // Since we're in an observation scope - this log line will contain tracing MDC entries ...
+							String response = restTemplate.getForObject("http://localhost:7654/user/{userId}", String.class, highCardinalityUserId); // Boot's RestTemplate instrumentation creates a child span here
+							//String response = restTemplate.getForObject("http://192.168.7.126:7654/user/{userId}", String.class, highCardinalityUserId); // Boot's RestTemplate instrumentation creates a child span here
+							log.info("Got response [{}]", response); // ... so will this line
+						});
+				Thread.sleep(highCardinalityValues.nextLong(500));
+			}
+
+		};
+	}
+	// end::runner[]
+
+	Random randomUserTypePicker = new Random();
+
+	private String randomUserTypePicker(List<String> lowNumberOfValues) {
+		return lowNumberOfValues.get(randomUserTypePicker.nextInt(2));
+	}
+}
---
+++ observability-client/src/test/java/net/javaguides/observability_client/ObservabilityClientApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.observability_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
